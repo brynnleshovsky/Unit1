808cdfc8bd33f47fa9f9833d6c4000b91e26be35
brynnleshovsky <815990@mystma.org>
2020-09-17 20:13:35 +0000

    Top 10 Movies

---
+++ Unit1/src/app/App.java
@@ -0,0 +1,16 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("1. Harry Potter series");
+        System.out.println("2. Divergent series");
+        System.out.println("3. Moana");
+        System.out.println("4. Mulan");
+        System.out.println("5. Napoleon Dynamite");
+        System.out.println("6. coco");
+        System.out.println("7. The Hunger Games series");
+        System.out.println("8. Home Alone");
+        System.out.println("9. Alice in Wonderland");
+        System.out.println("10. Dumb and Dumber");
+    }
+}
