2607ac04029c4978ef5351905f47d8e4cf83b4c9
brynnleshovsky <815990@mystma.org>
2020-09-18 20:14:03 +0000

    Primitive variables

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,15 +2,30 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("1. Harry Potter series");
-        System.out.println("2. Divergent series");
-        System.out.println("3. Moana");
-        System.out.println("4. Mulan");
-        System.out.println("5. Napoleon Dynamite");
-        System.out.println("6. coco");
-        System.out.println("7. The Hunger Games series");
-        System.out.println("8. Home Alone");
-        System.out.println("9. Alice in Wonderland");
-        System.out.println("10. Dumb and Dumber");
+      int coolNum = 1567;
+      System.out.println("What is a cool number?");
+      System.out.println(coolNum);
+      boolean hasBluEye= false;
+      System.out.println("I have blue eyes");
+      System.out.println(hasBluEye);
+      char firstLetterOfState = 'M';
+      System.out.println("What's the first letter of our state?");
+      System.out.println(firstLetterOfState);
+      byte numOfSibs = 1;
+      System.out.println("How many siblngs do I have?");
+      System.out.println(numOfSibs);
+      short popOftheTown = 17892;
+      System.out.println("How many people live in our town?");
+      System.out.println(popOftheTown);
+      long popOfChina = 1439323776;
+      System.out.println("How many people live in China?");
+      System.out.println(popOfChina);
+      float halfOfThr = 1.5f;
+      System.out.println("Hey, what's half of three?");
+      System.out.println(halfOfThr);
+      double firDigOfPi = 3.14159265;
+      System.out.println("Tell me the first 9 digits of pi.");
+      System.out.println(firDigOfPi);
+
     }
 }
