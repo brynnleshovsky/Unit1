de283af06644b99eaf710ca5a3a2f960159ff90f
brynnleshovsky <815990@mystma.org>
2020-09-22 18:07:02 +0000

    mad lib assignment

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,13 +2,26 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String myName = "Brynn Leshovsky";
-        System.out.println(myName);
-        String firstName = "Brynn";
-        String lastName ="Leshovsky";
-        System.out.println(firstName + " " + lastName) ; 
-        
+    String person1 = "my cat";
+    String person2 = "Jeff Bezos";
+    String verb1 = "programming";
+    String verb2 = "cleaning";
+    String verb3 = "watching harry potter";
+    String food = "lutefisk";
+    String dayOfWeek = "Tuesdays";
+    String place1 = "Tahiti";
+    String place2 = "jail";
+    String adjective1 = "gross";
+    String adjective2 = "amazing";
+    System.out.println("The world's richest person is " + person1 + ".");
+    System.out.println(person1 + " spends their time " + verb3 + " in " + place1 + ".");
+    System.out.println(verb1 + " is something they are extremely gifted in.");
+    System.out.println(person1 + " is so very " + adjective2 + ".");
+    System.out.println(person2 + " on the other hand is so " + adjective1 + " and they are very poor.");
+    System.out.println("On " + dayOfWeek + " they spend hours " + verb2 + " for " + person1 + ".");
+    System.out.println(person2 + " lives in " + place2 + " and only gets to eat "+ food + ".");
 
 
+     
     }
 }
