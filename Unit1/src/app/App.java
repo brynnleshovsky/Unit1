99f8d43357a5e339b1fdb6bb41e49e119121fc64
brynnleshovsky <815990@mystma.org>
2020-09-22 03:04:40 +0000

    Strings and Concatenation Lesson

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,30 +2,13 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-      int coolNum = 1567;
-      System.out.println("What is a cool number?");
-      System.out.println(coolNum);
-      boolean hasBluEye= false;
-      System.out.println("I have blue eyes");
-      System.out.println(hasBluEye);
-      char firstLetterOfState = 'M';
-      System.out.println("What's the first letter of our state?");
-      System.out.println(firstLetterOfState);
-      byte numOfSibs = 1;
-      System.out.println("How many siblngs do I have?");
-      System.out.println(numOfSibs);
-      short popOftheTown = 17892;
-      System.out.println("How many people live in our town?");
-      System.out.println(popOftheTown);
-      long popOfChina = 1439323776;
-      System.out.println("How many people live in China?");
-      System.out.println(popOfChina);
-      float halfOfThr = 1.5f;
-      System.out.println("Hey, what's half of three?");
-      System.out.println(halfOfThr);
-      double firDigOfPi = 3.14159265;
-      System.out.println("Tell me the first 9 digits of pi.");
-      System.out.println(firDigOfPi);
+        String myName = "Brynn Leshovsky";
+        System.out.println(myName);
+        String firstName = "Brynn";
+        String lastName ="Leshovsky";
+        System.out.println(firstName + " " + lastName) ; 
+        
+
 
     }
 }
