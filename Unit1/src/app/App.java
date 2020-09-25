8f9f0035156825e00c6efb25be2731bbe4d40e89
brynnleshovsky <815990@mystma.org>
2020-09-25 20:00:54 +0000

    While statement practice

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,25 +1,30 @@
 package app;
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-   Scanner input = new Scanner(System.in);
+        int count = 0;
+        String color = "red";
+        boolean Friday = true;
 
-   System.out.println("Please enter your first name.");
-   String name  = input.nextLine();
 
-   System.out.println("Hello, " + name);
-   System.out.println("Please enter your last name.");
-   String lastName = input.nextLine();
-   System.out.println("Nice to meet you, " + name + " " + lastName);
-   System.out.println("Please enter your age , " + name);
-   String age = input.nextLine();
-   System.out.println("thanks for sharing.");
-   System.out.println("Now, what grade are you in?");
-   String grade = input.nextLine();
-   System.out.println("Great! We are almost done, " + name);
-   System.out.println("One last thing, what is your favorite color?");
-   String favColor = input.nextLine(); 
-   System.out.println("Alright " + name + " " + lastName + "," + " you are " +  age + ", in grade " + grade + ", and your favorite color is " + favColor + ".");
-    }
+        while (count < 10) {
+            System.out.println("The count is " + count);
+            count = count + 1;
+        }
+
+        System.out.println("count finished");
+
+        while (color == "red"){
+            System.out.println("The color is " + color);
+        color = "green";
+        }
+
+        System.out.println("Color finished");
+
+        while (Friday == true){
+            System.out.println("Is today Friday? " + Friday);
+            Friday = false;
+        }
+        System.out.println("Friday is finished.");
+}
 }
