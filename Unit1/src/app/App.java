0c83292102493a229bcfeeb8104bf121d25d0760
brynnleshovsky <815990@mystma.org>
2020-09-30 03:35:39 +0000

    loop assignment

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -4,22 +4,34 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-      
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Please enter a color.");
-        String color = input.nextLine();
-
-        System.out.println("Please enter a number.");
-        int number = input.nextInt();
-
-        int count = 0;
-
-        while(count < number){
-
-            System.out.println(color);
-            count = count + 1;
+        for (int i = 0; i <= 10; i = i + 1){
+            System.out.println("The count is " + i);
+            System.out.println("Let's keep counting.");
         }
-        
-}
+
+
+        for (int c = 10; c <= 20; c = c + 1){
+            System.out.println("The count is " + c);
+        }
+
+        for (int f = 10; f <= 100; f = f + 1){
+            System.out.println("The count is " + f);
+        }
+
+        for (int b = 1000; b <= 256000; b = b + 1){
+            System.out.println("The count is " + b);
+        }
+
+        for (int j = 11; j <= 12; j = j + 1){
+            System.out.println("The count is " + j);
+        }
+
+        for (int p = 10; p >= 0; p = p - 1){
+            System.out.println("The count is " + p);
+        }
+
+
+
+     }
 }
