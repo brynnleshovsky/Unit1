24116b11593eecf45851c5c3e8994a213ec25fd8
brynnleshovsky <815990@mystma.org>
2020-10-01 20:18:39 +0000

    quiz

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -4,34 +4,21 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
 
-        for (int i = 0; i <= 10; i = i + 1){
-            System.out.println("The count is " + i);
-            System.out.println("Let's keep counting.");
+
+        System.out.println("Question 1: how many siblings do I have?");
+        int sibs = input.nextInt();
+        int sibsA = 1;
+
+        if (sibs == sibsA){
+            System.out.println("Correct.");
         }
 
-
-        for (int c = 10; c <= 20; c = c + 1){
-            System.out.println("The count is " + c);
+        else{
+            System.out.println("incorrect");
         }
-
-        for (int f = 10; f <= 100; f = f + 1){
-            System.out.println("The count is " + f);
-        }
-
-        for (int b = 1000; b <= 256000; b = b + 1){
-            System.out.println("The count is " + b);
-        }
-
-        for (int j = 11; j <= 12; j = j + 1){
-            System.out.println("The count is " + j);
-        }
-
-        for (int p = 10; p >= 0; p = p - 1){
-            System.out.println("The count is " + p);
-        }
-
-
 
      }
 }
+
