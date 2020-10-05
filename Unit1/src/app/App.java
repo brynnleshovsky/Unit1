04b29ec2d4d0e7890315fc10ac9ff83ce5d0e34b
brynnleshovsky <815990@mystma.org>
2020-10-05 19:01:46 +0000

    Quiz plus five questions

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -8,6 +8,8 @@ public class App {
 
         int score = 0;
 
+        System.out.println("Welcome to your quiz. Before we start, any string answers must have the first letter capitalized to be counted correct. Let's begin.");
+
 
 
         System.out.println("Question 1: how many siblings do I have?");
@@ -76,8 +78,73 @@ public class App {
         else{
             System.out.println("Incorrect");
         }
-        System.out.println("Well done. You score is " + score + "/5");
+
+        System.out.println("Question 6: true or false? My favorite subject is math");
+        boolean favSub = input.nextBoolean();
+        input.nextLine();
+        boolean favSubA = false;
+
+        if(favSub == favSubA){
+            System.out.println("Correct.");
+            score = score + 1;
+        }
+
+        System.out.println("Question 7: What is my first pet's name?");
+        String firstPet = input.nextLine();
+        String firstPetA = "Earl";
+
+        if(firstPet.equals(firstPetA)){
+            System.out.println("Correct.");
+            score = score + 1;
+        }
+
+        else{
+            System.out.println("incorrect");
+        }
+
+        System.out.println("Question 8: What smell do I hate most?");
+        String hateSmell = input.nextLine();
+        String hateSmellA ="Pickles";
+
+        if(hateSmell.equals(hateSmellA)){
+            System.out.println("Correct.");
+            score = score + 1;
+        }
+
+        else{
+            System.out.println("Incorrect.");
+        }
+
+        System.out.println("Question 9: True or False? I like the Ocean");
+        boolean ocean = input.nextBoolean();
+        boolean oceanA = false;
+
+        if(ocean == oceanA){
+            System.out.println("Correct.");
+            score = score + 1;
+        }
+
+        else{
+            System.out.println("Incorrect.");
+        }
+
+        System.out.println("Question 10: True or False? I can't think of another question");
+        boolean thinkQ = input.nextBoolean();
+        boolean thinkQA = true;
+
+        if(thinkQ == thinkQA){
+            System.out.println("Correct.");
+            score = score + 1;
+        }
+
+        else{
+            System.out.println("Incorrect.");
+        }
+
+
+        System.out.println("Well done. You score is " + score + "/10");
 
      }
-}
+    }
+
 
