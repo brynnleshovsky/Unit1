244c4344004fcdcc645f56324510e40f8f3ed3fe
brynnleshovsky <815990@mystma.org>
2020-10-09 18:13:02 +0000

    Guess the Number Game

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,48 +5,26 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
+        int secretNum = 40;
+        int guess;
+        boolean answer = false;
 
-        System.out.println("Challenge 1: Print all numbers 1-1000 in as few lines as possible... starting NOW:");
 
-        for(int challenge1 = 1; challenge1<= 1000; challenge1 ++){
-          System.out.println(challenge1);  
+        while (!answer) {
+            System.out.println("Please guess a number.");
+            guess = input.nextInt();
+
+            if (guess == secretNum){
+                answer = true;
+                System.out.println("You guessed correctly!");
+            }
+            else if (guess < secretNum){
+                System.out.println("guess higher!");
+            }
+
+                else if (guess > secretNum){
+                    System.out.println("Guess lower!");
+                }
         }
-
-        System.out.println("Great! Challenge One is complete.");
-        System.out.println("Challenge 2: The secret Message. Enter the passcode to see the top secret information. Don't Guess too many times or else you may never know what the secret is...");
-        System.out.println("Enter the passcode.");
-        String attempt = input.nextLine();
-        String passcode = "Jelly Belly jelly beans";
-        String secret = "Jelly Belly jelly beans are actually really gross, why did you guess that passcode?";
-
-        if(attempt.equals(passcode)){
-            System.out.println("Correct! The secret is that " + secret + ". Anyway, you did it! have a great rest of your day.");
+            }
         }
-
-        else{
-            System.out.println("Error! The password you have entered is incorrect. You have two more attempts.");
-            System.out.println("Please enter the correct passcode.");
-            String attempt2 = input.nextLine();
-            if(attempt2.equals(passcode)){
-                System.out.println("Correct! The secret is that " + secret + ". Anyway, you did it! have a great rest of your day.");
-            }
-
-            else{
-                System.out.println("Error! Failed attempt 2. You have one more attempt.");
-                System.out.println("Please enter the correct passcode.");
-                String attempt3 = input.nextLine();
-                 if(attempt3.equals(passcode)){
-                    System.out.println("Correct! the secret is that " + secret + ". Anyway, you did it! have a great rest of your day.");
-                
-            }
-            else{
-                System.out.println("Error! You have failed 3 times. Due to security of the top secret message, I will have to end the program. Good Bye.");
-            }
-
-
-     }
-    }
-}
-}
-
-
