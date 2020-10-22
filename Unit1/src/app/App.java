0aa89942848a6d6c6ccf8daf6673e1cdb38e672a
brynnleshovsky <815990@mystma.org>
2020-10-22 19:59:53 +0000

    Quiz 1

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,25 +5,17 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        System.out.println("Hi! Please input any number");
-        int num1 = input.nextInt();
-        input.nextLine();
-        System.out.println("Please enter a higher number. I will print out all of the even numbers between the two you input.");
-        int num2 = input.nextInt();
+        int freezing = 32;
+        System.out.println("What is the current temp?");
+        int temp = input.nextInt();
         input.nextLine();
 
-        while (num1 < num2 -2){
-                if(num1 % 2 == 0){
-                    num1 = num1 +2;
-                    
-            System.out.println(num1);
-                    }
-
-            else{
-                num1 = num1 +1;
-                System.out.println(num1);
-            }
+        if(temp <= freezing){
+            System.out.println("It is freezing.");
         }
-        System.out.println("That's it! Press run to play again."); 
+
+        else{
+            System.out.println("It is above freezing.");
+        }
             }
         }
