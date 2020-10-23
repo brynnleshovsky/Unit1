3ae96330b34b8b2c3a41aa0f9878b7e3afc55cd1
brynnleshovsky <815990@mystma.org>
2020-10-23 20:14:37 +0000

    Barely started roulette

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,17 +5,10 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        int freezing = 32;
-        System.out.println("What is the current temp?");
-        int temp = input.nextInt();
-        input.nextLine();
 
-        if(temp <= freezing){
-            System.out.println("It is freezing.");
-        }
-
-        else{
-            System.out.println("It is above freezing.");
+        System.out.println("Welcome to roulette. You will start with 100 $1 chips");
+        int chips = 100;
+       
         }
             }
-        }
+        
