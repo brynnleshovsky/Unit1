9ea9f9aec12c8f50f8d67ac821e05c714d84abb4
brynnleshovsky <815990@mystma.org>
2020-10-30 19:40:59 +0000

    Array overview

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,58 +5,16 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-
-        System.out.println("Welcome to roulette. You will start with 100 $1 chips");
-        int chips = 100;
-         System.out.println("Hi and welcome to roulette.");
-         System.out.println("Would you like to bet inside or outside?");
-         String inOrOut = input.nextLine();
-         String in = "inside";
-         String out = "outside";
-         int remainingBets = 10;
-         String yes = "yes";
-         String no = "no";
-         boolean looping = true;
-         String red = "red";
-         String black = "black";
-         String odd= "odd";
-         String even = "even";
-
-
-         if (inOrOut.equals(in)){
-             System.out.println("Enter the number you would like to bet on.");
-             int betNum = input.nextInt();
-             input.nextLine();
-             System.out.println("And how much would you like to bet?");
-             int betMon = input.nextInt();
-             input.nextLine();
-             System.out.println("Okay. You will bet $" + betMon + " on " + betNum + ".");
-             remainingBets = remainingBets -1;
-         }
-
-             else{
-                 System.out.println("would you like to bet even, odd, black, or red?");
-                 String outBet = input.nextLine();
-                 if (outBet.equals(red)){}
-                 if (outBet.equals(black)){}
-                 if (outBet.equals(odd)){}
-                 if (outBet.equals(even)){}
-
-             }
-         
-         while (looping == true){
-         System.out.println("Would you like to make another bet? You have " + remainingBets + " left.");
-         String secondBet = input.nextLine();
-         if (secondBet.equals(yes)){
-             System.out.println("check");
-         }
-         else if (secondBet.equals(no)){
-             looping = false;
-             System.out.println("Betting is now complete.");
-         }
+        String line[] = {"Diddy", "Daisy", "Yoshi", "Toad", "Wario","DK", "Luigi", "Peach", "Mario", "Waluigi"};
+        System.out.println(line[4]);
+        System.out.println(line[0]);
+        line[4]= "Bowser";
+        System.out.println(line[4]);
+        int numOfPeepsInLine = line.length;
+        for (int i = 0; i < line.length; i++){
+            System.out.println(i);
         }
-
-       
-        }
+        
             }
+        }
         
