9068d7e8b0b468ef082339e33a34119921ff293a
brynnleshovsky <815990@mystma.org>
2020-11-06 21:10:25 +0000

    multiply 4 nums and less than 10

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,20 +1,20 @@
 package app;
-
 import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        String line[] = {"Diddy", "Daisy", "Yoshi", "Toad", "Wario","DK", "Luigi", "Peach", "Mario", "Waluigi"};
-        System.out.println(line[4]);
-        System.out.println(line[0]);
-        line[4]= "Bowser";
-        System.out.println(line[4]);
-        int numOfPeepsInLine = line.length;
-        for (int i = 0; i < line.length; i++){
-            System.out.println(i);
+        multiply(2,4,1,6);
+         }
+         public static void greaterOrLess10(int number){
+        if (number <= 10){
+            System.out.println(number + " is less than ten.");
         }
-        
-            }
+        else{
+            System.out.println(number + " is greater than 10.");
+        }
+         }
+         public static void multiply(int num1, int num2, int num3, int num4){
+             System.out.println(num1 * num2 * num3 * num4);
+         }
         }
         
