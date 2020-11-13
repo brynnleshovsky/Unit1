9e9b828ade59714d324a2e2a85d75a1f3df062ff
brynnleshovsky <815990@mystma.org>
2020-11-13 21:10:42 +0000

    Classes Dog practice

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,18 +3,22 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        multiply(2,4,1,6);
-         }
-         public static void greaterOrLess10(int number){
-        if (number <= 10){
-            System.out.println(number + " is less than ten.");
+        Dog dog1 = new Dog();
+        dog1.name = "Buddy";
+        dog1.age = 5;
+        dog1.breed = "Bulldog";
+        System.out.println("Our first dog is named " + dog1.name + "." + " " + dog1.name + " is a " + dog1.age + " year old " + dog1.breed + ".");
+        Dog dog2 = new Dog();
+        dog2.name = "Fluffy";
+        dog2.age = 1;
+        dog2.breed = "Pug";
+        System.out.println("Our second dog is named " + dog2.name + ". " + dog2.name + " is a " + dog2.age + " year old " + dog2.breed + ".");
+        Dog dog3 = new Dog();
+        dog3.name = "Mr. Cuddles";
+        dog3.age = 12;
+        dog3.breed = "Australian Shepherd";
+        System.out.println("Our third dog is named " + dog3.name + ". " + dog3.name + " is a " + dog3.age + " year old " + dog3.breed + ".");
+
         }
-        else{
-            System.out.println(number + " is greater than 10.");
-        }
-         }
-         public static void multiply(int num1, int num2, int num3, int num4){
-             System.out.println(num1 * num2 * num3 * num4);
-         }
-        }
+    }
         
---
+++ Unit1/src/app/Dog.java
@@ -0,0 +1,7 @@
+package app;
+
+public class Dog {
+    String name;
+    String breed;
+    int age;
+}
